e430a3610811ceccce5307c18599698115a6e233
weixing06 <weixings06@126.com>
2020-10-27 09:27:59 +0800

    first commit

---
+++ hw.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+int main(){
+    using namespace std;
+    cout << "come up and C++ me some time";
+    cout << endl;
+    cout << "you wont regret it" << endl;
+
+
+    int carrots;
+    cout << "How many carrots do you have?" <<endl;
+    cin >> carrots;
+    cout << "here are tow more";
+    carrots += 2;
+    cout << "Now you have " << carrots << " carrots." << endl;
+
+    return 0;
+}
